c46466addfc2c79736b4cdfad61da0447b13eadc
mehran-bangash <221029@students.au.edu.pk>
2024-10-26 00:51:29 +0500

    intial update

--- src/App.js
+++ src/App.js
@@ -6,17 +6,13 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h3>
+          Mehran Ali
+        </h3>
+         <h3>
+          Roll No: 221029
+        </h3>
+       
       </header>
     </div>
   );
